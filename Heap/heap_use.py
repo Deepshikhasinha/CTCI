b86b55a52c47fd8167baa5d08d98c8a464eebd2b
Deepshikha Sinha <deep.tgmc@gmail.com>
2019-01-17 10:32:57 -0500

    Create heap_use.py

---
+++ Heap/heap_use.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+list1 = [[5,7,9],[1,4,8,10],[6,11,18]]
+#list2 = 
+#list3 = 
+#list_all = list1+list2+list3
+for i in range(len(list1)):
+    listk.append(list1[i][0])
+    del list1[i][0]
+heapq.heapify(listk)
+array = []
+p1 = 0
+p2 = 0
+p3 = 0
+while listk:
+    array.append()
+    heapq.heappush(listk,list1[p1])
+print(heapq.nlargest(5, list_all))
